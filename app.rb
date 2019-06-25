1b089f8b68431f167dd11f6b1fdff3959382860c
David Anderson <matandersonlv@hotmail.com>
2019-06-25 17:12:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,7 +23,7 @@ class App < Sinatra::Base
   
   get "/say/:word1/:word2/:word3/:word4/:word5" do
     combined_string = params[:word1] + params[:word1] + params[:word1] + params[:word1] + params[:word1]
-    
+    combined_string
   end
   
 end
