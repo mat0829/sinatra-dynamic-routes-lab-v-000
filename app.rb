86200a97446392d578634841d7e062aad25fd08e
David Anderson <matandersonlv@hotmail.com>
2019-06-25 17:30:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -26,7 +26,12 @@ class App < Sinatra::Base
   end
   
   get "/:operation/:number1/:number2" do
-    params[:number1].to_i params[:operation] params[:number2]
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+    
+    answer = ""
+    
+    when answer = 
   end
   
   
